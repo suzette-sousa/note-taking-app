c17094994b5b5f51e7b3822ea33d3c1fa8946a19
Suzette Sousa <sousa.suzette.pro@gmail.com>
2020-12-27 09:37:31 +0100

    initial config

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.get('/notes', (req, res) => res.send('Hello World'));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
